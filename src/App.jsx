a46ba0f380eba5d768f5adb13e100be4111b9ce6
JuanjDes <juanjguapo@gmail.com>
2025-04-06 15:15:13 +0200

    Ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react"
 import './App.css'
 
+// Función App() sólo con 3 botones para cambiar el nombre
+/* function App() {
+    const [name, setName] = useState('Sofía');
+    const [newName, setNewName] = useState('');
+  
+    return (
+        <div>
+            <h2>Teacher name: {name}</h2>
+            <hr></hr>
+            <button onClick={() => setName('Data')}>Data</button>
+            <button onClick={() => setName('Reyes')}>Reyes</button>
+            <button onClick={() => setName('Yolanda')}>Yolanda</button>
+        </div>
+    )
+} */
+
+// Función App() con formulario para cambiar el nombre
 function App() {
- //Aquí tu código
+    const [name, setName] = useState('Sofía');
+    const [newName, setNewName] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Evita que la página se recargue
+        if (newName.trim()) {   // Verifica que no esté vacío
+          setName(newName);
+          setNewName('');       // Limpia el input después de enviar
+        }
+      };
+  
+    return (
+        <div>
+            <h2>Teacher name: {name}</h2>
+            <hr></hr>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={newName}
+                    onChange={(e) => setNewName(e.target.value)}
+                    placeholder="Enter new teacher name"/>
+                <button type="submit">Change Name</button>
+            </form>
+        </div>
+    )
 }
 
 export default App
